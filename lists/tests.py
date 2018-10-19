c8b829ae2c0691ab3a502e0ac7d656af2a1ab381
Illya Kaynov <ikaynov@gmail.com>
2018-10-19 16:02:10 +0200

    commiting to continue on a PC

--- lists/tests.py
+++ lists/tests.py
@@ -18,6 +18,6 @@ class SmokeTest(TestCase):
     def test_can_save_POST_request(self):
         response = self.client.post('/', data={'item_text': 'A new list item'})
         self.assertIn('A new list item', response.content.decode())
-
+        self.assertTemplateUsed(response, 'home.html')
         
 
