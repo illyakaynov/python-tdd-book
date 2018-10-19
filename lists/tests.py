ce8036c95112ccbaf8cdf1cb129db2b43cb79272
illyakaynov <ikaynov@gmail.com>
2018-10-19 08:45:57 +0200

    getting ready to save user input

--- lists/tests.py
+++ lists/tests.py
@@ -14,6 +14,10 @@ class SmokeTest(TestCase):
         html = response.content.decode('utf8')
 
         self.assertTemplateUsed(response, 'home.html')
-       
+    
+    def test_can_save_POST_request(self):
+        response = self.client.post('/', data={'item_text': 'A new list item'})
+        self.assertIn('A new list item', response.content.decode())
+
         
 
