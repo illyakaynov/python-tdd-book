5bafd206cc18ed879ea77d95da0383e0baccdb3b
illyakaynov <ikaynov@gmail.com>
2018-10-22 15:52:12 +0200

    Redirect after POST, and show all items in a  template

--- functional_tests.py
+++ functional_tests.py
@@ -38,30 +38,27 @@ class NewVisitorTest(unittest.TestCase):
 
         # She types "Buy peacock feathers" into a text box (Edith's hobby
         # is tying fly-fishing lures)
-        inputbox.send_keys('Buy peacock feathers')  
+        inputbox.send_keys('Buy peacock feathers')
 
         # When she hits enter, the page updates, and now the page lists
         # "1: Buy peacock feathers" as an item in a to-do list table
-        inputbox.send_keys(Keys.ENTER)  
+        inputbox.send_keys(Keys.ENTER)
         time.sleep(1)
-        
         self.check_for_row_in_list_table('1: Buy peacock feathers')
-        self.check_for_row_in_list_table('2: Use peacock feathers to make a fly')
 
         # There is still a text box inviting her to add another item. She
         # enters "Use peacock feathers to make a fly" (Edith is very
         # methodical)
         inputbox = self.browser.find_element_by_id('id_new_item')
         inputbox.send_keys('Use peacock feathers to make a fly')
-        inputbox.send_keys(Key.ENTER)
+        inputbox.send_keys(Keys.ENTER)
         time.sleep(1)
 
-
+        # The page updates again, and now shows both items on her list
         self.check_for_row_in_list_table('1: Buy peacock feathers')
         self.check_for_row_in_list_table('2: Use peacock feathers to make a fly')
-        self.fail('Finish the test!')
 
-        # The page updates again, and now shows both items on her list
+        # Edith wonders whether the site will remember her list. Then she sees
 
 if __name__=='__main__':
     unittest.main(warnings='ignore')
