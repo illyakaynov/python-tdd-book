2d9c1b6f7d0ec5bf992bd902c15650e66f90380e
illyakaynov <ikaynov@gmail.com>
2018-10-29 19:36:44 +0100

    3 changed files, plus 2 migrations added lists

--- lists/tests.py
+++ lists/tests.py
@@ -2,25 +2,39 @@ from django.test import TestCase
 from django.urls import resolve
 from lists.views import home_page
 from lists.models import Item
+from lists.models import List
 
 from django.http import HttpRequest
+
+
 # Create your tests here.
 
 class SmokeTest(TestCase):
-    
+
     def test_only_saves_items_when_neccessary(self):
         self.client.get('/')
         self.assertEqual(Item.objects.count(), 0)
 
-
     def test_saving_and_retrieving_items(self):
+        list_ = List()
+
+        list_.save()
+
         first_item = Item()
         first_item.text = 'The first (ever) list item'
+
+        first_item.list = list_
+
+
         first_item.save()
 
         second_item = Item()
         second_item.text = 'Item the second'
+        second_item.list = list_
         second_item.save()
+
+        saved_list = List.objects.first()
+        self.assertEqual(saved_list, list_)
 
         saved_items = Item.objects.all()
         self.assertEqual(saved_items.count(), 2)
@@ -28,24 +42,30 @@ class SmokeTest(TestCase):
         first_saved_item = saved_items[0]
         second_saved_item = saved_items[1]
         self.assertEqual(first_saved_item.text, 'The first (ever) list item')
+        self.assertEqual(first_saved_item.list, list_)
         self.assertEqual(second_saved_item.text, 'Item the second')
+        self.assertEqual(second_saved_item.list, list_)
 
-    def test_home_page_returns_correct_html(self):
-        response = self.client.get('/')
-        self.assertTemplateUsed(response, 'home.html')
 
-    def test_uses_list_template(self):
-        response = self.client.get('/lists/the-only-list-in-the-world/')
-        self.assertTemplateUsed(response, 'list.html')
+def test_home_page_returns_correct_html(self):
+    response = self.client.get('/')
+    self.assertTemplateUsed(response, 'home.html')
 
-    def test_displays_all_items(self):
-        Item.objects.create(text='itemey 1')
-        Item.objects.create(text='itemey 2')
 
-        response = self.client.get('/lists/the-only-list-in-the-world/')
+def test_uses_list_template(self):
+    response = self.client.get('/lists/the-only-list-in-the-world/')
+    self.assertTemplateUsed(response, 'list.html')
 
-        self.assertContains(response, 'itemey 1')
-        self.assertContains(response, 'itemey 2')
+
+def test_displays_all_items(self):
+    Item.objects.create(text='itemey 1')
+    Item.objects.create(text='itemey 2')
+
+    response = self.client.get('/lists/the-only-list-in-the-world/')
+
+    self.assertContains(response, 'itemey 1')
+    self.assertContains(response, 'itemey 2')
+
 
 class NewListTest(TestCase):
     def test_can_save_POST_request(self):
@@ -57,4 +77,12 @@ class NewListTest(TestCase):
     def test_redirects_after_POST(self):
         response = self.client.post('/lists/new', data={'item_text': 'A new list item'})
         self.assertRedirects(response, '/lists/the-only-list-in-the-world/')
+
+
+class ListViewTest(TestCase):
+
+    def test_displays_all_times(self):
+        list_ = List.objects.create()
+        Item.objects.create(text='itemey 1', list=list_)
+        Item.objects.create(text='itemey 2', list=list_)
 
