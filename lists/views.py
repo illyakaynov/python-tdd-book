e1bb960055d877798cf636f372fe38e0734bb283
illyakaynov <ikaynov@gmail.com>
2018-10-30 08:46:41 +0100

    added few tests -> laptop

--- lists/views.py
+++ lists/views.py
@@ -7,7 +7,7 @@ from lists.models import List
 def home_page(request):
     return render(request, 'home.html')
 
-def view_list(request):
+def view_list(request, list_id):
     items = Item.objects.all()
     return render(request, 'list.html', {'items': items})
 
