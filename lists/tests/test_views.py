0135465dfbf76ee79f97e26a38e2675eba8f2681
Illya Kaynov <ikaynov@gmail.com>
2018-11-05 09:38:46 +0100

    split unitest into multiple files

--- lists/tests/test_views.py
+++ lists/tests/test_views.py
@@ -9,50 +9,6 @@ from django.http import HttpRequest
 
 # Create your tests here.
 
-class SmokeTest(TestCase):
-
-    def test_only_saves_items_when_neccessary(self):
-        self.client.get('/')
-        self.assertEqual(Item.objects.count(), 0)
-
-    def test_saving_and_retrieving_items(self):
-        list_ = List()
-
-        list_.save()
-
-        first_item = Item()
-        first_item.text = 'The first (ever) list item'
-
-        first_item.list = list_
-
-
-        first_item.save()
-
-        second_item = Item()
-        second_item.text = 'Item the second'
-        second_item.list = list_
-        second_item.save()
-
-        saved_list = List.objects.first()
-        self.assertEqual(saved_list, list_)
-
-        saved_items = Item.objects.all()
-        self.assertEqual(saved_items.count(), 2)
-
-        first_saved_item = saved_items[0]
-        second_saved_item = saved_items[1]
-        self.assertEqual(first_saved_item.text, 'The first (ever) list item')
-        self.assertEqual(first_saved_item.list, list_)
-        self.assertEqual(second_saved_item.text, 'Item the second')
-        self.assertEqual(second_saved_item.list, list_)
-
-
-    def test_home_page_returns_correct_html(self):
-        response = self.client.get('/')
-        self.assertTemplateUsed(response, 'home.html')
-
-
-
 
 class NewListTest(TestCase):
     def test_can_save_POST_request(self):
