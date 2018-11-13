539438a761673baf6f1aad9ddf60320193ff8c94
Illya Kaynov <ikaynov@gmail.com>
2018-11-13 14:53:58 +0100

    added unitest for email sending, before monkeypatching

--- superlists/urls.py
+++ superlists/urls.py
@@ -19,8 +19,10 @@ from django.conf.urls import include, url
 from lists import views as list_views
 from lists import urls as list_urls
 
+from accounts import urls as accounts_url
 urlpatterns = [
     url(r'^$', list_views.home_page, name='home'),
     url(r'^lists/', include(list_urls)),
+    url(r'^accounts/', include(accounts_url))
 ]
 
