03d906088afab77dd62d91afd1efd63158e676e1
illyakaynov <ikaynov@gmail.com>
2018-11-15 10:06:11 +0100

    added tests for login, checking token generation and email assignment

--- accounts/views.py
+++ accounts/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
-
+from django.core.urlresolvers import reverse
+from accounts.models import Token
 # Create your views here.
 from django.core.mail import send_mail
 from django.shortcuts import redirect
@@ -7,9 +8,14 @@ from django.contrib import messages
 
 def send_login_email(request):
     email = request.POST['email']
+    token = Token.objects.create(email=email)
+    url = request.build_absolute_uri(
+        reverse('login') + '?token=' + str(token.uid)
+    )
+    message_body = f'Use this link to log in:\n\n{url}'
     send_mail(
         'Your login link for Superlists',
-        'body text tbc',
+        message_body,
         'noreply@superlists',
         [email]
     )
@@ -23,4 +29,8 @@ def send_login_email(request):
         request,
         'Check your email, we have sent you a link you can use to log in'
     )
+    return redirect('/')
+
+
+def login(request):
     return redirect('/')
